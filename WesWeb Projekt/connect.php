797d4ea2c4ebcab35077441badd7059db8404664
EmrikSkriverKod <159128999+EmrikSkriverKod@users.noreply.github.com>
2024-02-06 11:18:17 +0100

    Update connect.php

--- WesWeb Projekt/connect.php
+++ WesWeb Projekt/connect.php
@@ -1,7 +1,6 @@
 
 
 <?php
-// db_connect.php
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";
@@ -13,26 +12,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-// if($_SERVER['REQUEST_METHOD'] == 'POST' && isSet($_POST['submit'])) {
-//     $conn= mysqli_connect('localhost', 'root', '', 'gothia' ) or die("Connection Failed" .mysqli_connect_error());
-//     if(isset($_POST['namn']) && isset($_PSOT['epost']) && isset($_POST['mobil']) && isset($_POST['lagnamn'])) {
-//         $name= $_POST['namn'];
-//         $epost= $_POST['epost'];
-//         $mobil= $_POST['mobil'];
-//         $lagnamn= $_POST['lagnamn'];
-
-//         $sql= "INSERT INTO 'lag' ('namn', 'epost', 'mobil', 'lagnamn') VALUES ('$name', '$epost', '$mobil', '$lagnamn')";
-
-//         $query = mysqli_query($conn,$sql);
-//         if($query) {
-//             echo 'KLART';
-//         }
-//         else {
-//             echo 'FEL UPPSTOD';
-//         }
-    
-//     }
-
-// }
 ?>
